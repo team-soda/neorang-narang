b82486d5c175a2bd9863e0f9c02d82cb3196ffa1
daseul-jang <jds940823@naver.com>
2022-06-03 01:12:38 +0900

    feat: RestController로 변경

--- neorangnarang-back/src/main/java/com/team/neorangnarang/common/controller/IndexController.java
+++ neorangnarang-back/src/main/java/com/team/neorangnarang/common/controller/IndexController.java
@@ -1,34 +1,19 @@
 package com.team.neorangnarang.common.controller;
 
-import com.team.neorangnarang.mainboard.service.MainboardService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
-@Controller
+@RestController
 public class IndexController {
-
-    @Autowired
-    private MainboardService boardService;
 
     @GetMapping("/")
     public String getIndex() {
         // 아직 안 만들어둔 main 페이지
-        return "redirect:/main";
+        return "메인~~";
     }
 
-    @RequestMapping("/test")
-    public String firstMain(Model model){
-
-        String time = boardService.returnTime();
-        model.addAttribute("time", time);
-
-        log.info("컨트롤러 time 값??>>>"+time);
-
-        return "test";
-    }
 }
