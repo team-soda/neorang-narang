e6724bbd2c5246eeac920f79d99d86854f85b53c
daseul-jang <jds940823@naver.com>
2022-06-06 05:51:33 +0900

    feat: 소셜 로그인 구현 중

--- neorangnarang-front/src/App.js
+++
@@ -1,8 +0,0 @@
-import AppRouter from "./components/AppRouter";
-import Axios from "./config/axios-config";
-
-function App() {
-  return <div className="App">메인</div>;
-}
-
-export default App;
--- neorangnarang-front/src/components/AppRouter.js
+++ neorangnarang-front/src/components/AppRouter.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "../App";
+import App from "../routes/App";
 import Redirect from "../routes/Redirect";
 import Signin from "../routes/Signin";
 
