8466f542ede82567c9b20d9d0c0538ffb583fccd
daseul-jang <jds940823@naver.com>
2022-06-17 03:16:05 +0900

    refactor: 코드 개선

--- neorangnarang-front/src/components/board/ListComponent.js
+++ neorangnarang-front/src/components/board/ListComponent.js
@@ -1,81 +1,80 @@
-import React, {useEffect, useState} from "react";
-import {DataGrid} from "@mui/x-data-grid";
-import {Link} from "@material-ui/core";
-import {boardService} from "../../service/BoardService";
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Link } from "@material-ui/core";
+import { boardService } from "../../service/BoardService";
 
-const ListComponent = ({boardList,setBoardList}) => {
+const ListComponent = ({ boardList, setBoardList }) => {
+  // const boardInfoState = {
+  //     type: '',
+  //     keyword: '',
+  //     searchResult: []
+  // }
+  //
+  // const [boardList, setBoardList] = useState(boardInfoState)
 
-    // const boardInfoState = {
-    //     type: '',
-    //     keyword: '',
-    //     searchResult: []
-    // }
-    //
-    // const [boardList, setBoardList] = useState(boardInfoState)
+  useEffect(() => {
+    boardService
+      .getBoardList()
+      .then((res) => setBoardList(res.data.searchResult));
+  }, [setBoardList]);
 
-    useEffect(() => {
-        boardService
-            .getBoardList()
-            .then((res) => setBoardList(res.data.searchResult));
-    }, [setBoardList]);
+  const columns = [
+    {
+      field: "id",
+      headerName: "No.",
+      flex: 0.3,
+    },
+    {
+      field: "title",
+      headerName: "Title",
+      flex: 1.4,
+      renderCell: (id) => {
+        return (
+          <Link href={`/mainboard/read/${id.row.id}`}>{id.row.title}</Link>
+        );
+      },
+    },
+    {
+      field: "writer",
+      headerName: "Author",
+      flex: 0.8,
+    },
+    {
+      field: "like_count",
+      headerName: "♡",
+      type: "number",
+      flex: 0.3,
+    },
+    {
+      field: "created_at",
+      headerName: "Date",
+      flex: 1,
+    },
+  ];
 
-    const columns = [
-        {
-            field: "id",
-            headerName: "No.",
-            flex: 0.3,
-        },
-        {
-            field: "title",
-            headerName: "Title",
-            flex: 1.4,
-            renderCell: (id) => {
-                return (
-                    <Link href={`/mainboard/read/${id.row.id}`}>{id.row.title}</Link>
-                );
-            },
-        },
-        {
-            field: "writer",
-            headerName: "Author",
-            flex: 0.8,
-        },
-        {
-            field: "like_count",
-            headerName: "♡",
-            type: "number",
-            flex: 0.3,
-        },
-        {
-            field: "created_at",
-            headerName: "Date",
-            flex: 1,
-        },
-    ];
+  const rows = boardList.map((board) => {
+    return {
+      id: board.board_idx,
+      title: board.title,
+      writer: board.writer,
+      like_count: board.like_count,
+      created_at: board.created_at,
+    };
+  });
 
-    const rows = boardList.map((board) => {
-        return {
-            id: board.board_idx,
-            title: board.title,
-            writer: board.writer,
-            like_count: board.like_count,
-            created_at: board.created_at,
-        };
-    });
-
-    return (
-        <>
-            리스트
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={8}
-                disableSelectionOnClick
-                autoHeight={true}
-                disableExtendRowFullWidth={true}
-            />
-        </>
-    );
+  return (
+    <>
+      리스트
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={8}
+        disableSelectionOnClick
+        autoHeight={true}
+        disableExtendRowFullWidth={true}
+      />
+    </>
+  );
 };
 
 export default ListComponent;
